850eee474f75f1de9ea7df5c0a9f92493a07e455
Kevin Bost <kitokeboo@gmail.com>
2024-07-24 09:22:43 -0700

    Removing bad test

--- AutoTyper.Tests/ProgramTests.cs
+++ AutoTyper.Tests/ProgramTests.cs
@@ -14,16 +14,6 @@ public class ProgramTests
         Assert.Contains("--help", stdOut.ToString());
     }
 
-    [Fact]
-    public async Task Invoke_AddWithTwoNumbers_DisplaysResult()
-    {
-        using StringWriter stdOut = new();
-        int exitCode = await Invoke("add 4 2", stdOut);
-
-        Assert.Equal(0, exitCode);
-        Assert.Contains("The result is 6", stdOut.ToString());
-    }
-
     private static Task<int> Invoke(string commandLine, StringWriter console)
     {
         CliConfiguration configuration = Program.GetConfiguration();
